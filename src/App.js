c67a61a59a2a4e2e4416bc69925a86fd59697c15
Tadeusz Ciszewski <office@tc-soft.pl>
2024-03-25 20:33:52 +0100

    feat: Add toFixes() JS method to ammount, update package.json

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import CustomDatePicker from './components/CustomDatePicker';
+import CustomSlider from './components/CustomSlider';
+import { format, differenceInDays, addDays, getYear, startOfYear, endOfYear } from "date-fns";
 import './App.css';
 
 function App() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(addDays(new Date(), 365));
+  const [ratio, setRatio] = useState(1);
+  const [amount, setAmount] = useState(''); // Nowy stan dla wartości kwotowej
+
+  const handleStartDateChange = (date) => {
+    setStartDate(date);
+  };
+
+  const handleEndDateChange = (date) => {
+    setEndDate(date);
+  };
+
+  const handleRatioChange = (event, newValue) => {
+    setRatio(newValue);
+  };
+
+    const handleAmountChange = (event) => {
+    setAmount(event.target.value);
+  };
+
+  const startOfYearDate = startOfYear(endDate);
+  const endOfYearDate = endOfYear(startDate);
+  const daysLeftInYear = differenceInDays(endOfYearDate, startDate) + 1;
+  const daysAll = differenceInDays(endDate, startDate) + 1;
+
+  const isSameYear = getYear(startDate) === getYear(endDate);
+  const daysAfterNewYear = isSameYear ? 0 : differenceInDays(endDate, startOfYearDate) + 1;
+
+  const amountOldYear = parseFloat(((amount / daysAll) * daysLeftInYear * ratio).toFixed(2));
+  const amountNewYear = parseFloat(((amount / daysAll) * daysAfterNewYear * ratio).toFixed(2));
+
+  const totalAmount = amountOldYear + amountNewYear;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <div className="App">
+        <h1>DLA JOLI</h1>
+        <p>Data początkowa :</p>
+        <CustomDatePicker date={startDate} onChange={handleStartDateChange} />
+
+        <p>Data końcowa :</p>
+        <CustomDatePicker date={endDate} onChange={handleEndDateChange} />
+
+        <p>Ilość wszystkich dni: {daysAll}</p>
+        <p style={{ color: 'blue' }}>
+          Ilość dni do końca roku: <strong>{daysLeftInYear}</strong> - Ilość dni po nowym roku: <strong>{daysAfterNewYear}</strong>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        <p>Współczynnik: {ratio}</p>
+        <div className="custom-slider-container">
+          <CustomSlider
+            ratio={ratio}
+            onChange={handleRatioChange}
+          />
+        </div>
+        
+        <p>Wpisz kwotę:</p>
+        <input
+          type="number"
+          value={amount}
+          onChange={handleAmountChange}
+          placeholder="Wpisz kwotę"
+        />
+        <br/>
+
+        <p>Kwota na każdy dzień po zaokrągleniu : { ((amount / daysAll) * ratio).toFixed(2) } zł (suma to {((amount / daysAll) * ratio).toFixed(2) * daysAll} zł)</p>
+        <p>Kwota na stary rok: {amountOldYear} zł</p>
+        <p>Kwota na nowy rok: {amountNewYear} zł</p>
+        <p>SUMA: {amountOldYear + amountNewYear} zł</p>
+
+      </div>
+    </>
   );
 }
 
--- src/components/CustomDatePicker.js
+++ src/components/CustomDatePicker.js
@@ -1,13 +1,14 @@
 
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import { pl } from 'date-fns/locale';
 
 const CustomDatePicker = ({date, onChange}) => {
 
   return (
       <DatePicker
-        showIcon
         dateFormat="dd.MM.yyyy"
+        locale={pl}
         selected={date}
         onChange={onChange}
       />
